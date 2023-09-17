d595bb6bd16b0929c556e2505addf674b161dfce
MatthewDBrown50 <matthew.d.brown50@gmail.com>
2023-09-17 19:45:04 -0400

    Improved implementation with recursive feature.

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-
-function getSquareVertices(center, diameter){
-    return [
-        center[0] - diameter/2, center[1] + diameter/2,
-        center[0] + diameter/2, center[1] - diameter/2,
-        center[0] + diameter/2, center[1] + diameter/2,
-        center[0] - diameter/2, center[1] + diameter/2,
-        center[0] - diameter/2, center[1] - diameter/2,
-        center[0] + diameter/2, center[1] - diameter/2
-    ]    
-}
-
-function getNewCenters(originalCenter, originalDiameter){
-    let newCenters = [
-        originalCenter[0] - originalDiameter, originalCenter[1] + originalDiameter,
-        originalCenter[0], originalCenter[1] + originalDiameter,
-        originalCenter[0] + originalDiameter, originalCenter[1] + originalDiameter,
-        originalCenter[0] - originalDiameter, originalCenter[1],
-        originalCenter[0] + originalDiameter, originalCenter[1],
-        originalCenter[0] - originalDiameter, originalCenter[1] - originalDiameter,
-        originalCenter[0], originalCenter[1] - originalDiameter,
-        originalCenter[0] + originalDiameter, originalCenter[1] - originalDiameter,
-    ]
-
-    return newCenters;
-}
-
 const canvas = document.querySelector('#c');
 const gl = canvas.getContext('webgl');
 
@@ -33,18 +6,52 @@ const vertexShaderSource = `
     void main() {
         gl_Position = vec4(aPosition, 0.0, 1.0);
     }
-    `
-;
+`;
 
 const fragmentShaderSource = `
     precision mediump float;
     uniform vec4 uColor;
 
     void main() {
-      gl_FragColor = uColor;
+        gl_FragColor = uColor;
     }
-    `
-;
+`;
+
+function getSquareVertices(center, diameter) {
+    return [
+        center[0] - diameter / 2, center[1] + diameter / 2,
+        center[0] + diameter / 2, center[1] + diameter / 2,
+        center[0] - diameter / 2, center[1] - diameter / 2,
+        center[0] + diameter / 2, center[1] - diameter / 2,
+        center[0] - diameter / 2, center[1] - diameter / 2,
+        center[0] + diameter / 2, center[1] + diameter / 2,
+    ];
+}
+
+function drawSierpinski(center, diameter, steps) {
+    if (steps === 0) {
+        return getSquareVertices(center, diameter);
+    }
+
+    const newDiameter = diameter / 3;
+    const newCenters = [
+        [center[0] - newDiameter, center[1] + newDiameter],
+        [center[0], center[1] + newDiameter],
+        [center[0] + newDiameter, center[1] + newDiameter],
+        [center[0] - newDiameter, center[1]],
+        [center[0] + newDiameter, center[1]],
+        [center[0] - newDiameter, center[1] - newDiameter],
+        [center[0], center[1] - newDiameter],
+        [center[0] + newDiameter, center[1] - newDiameter]
+    ];
+
+    let vertices = [];
+    for (const newCenter of newCenters) {
+        vertices = vertices.concat(drawSierpinski(newCenter, newDiameter, steps - 1));
+    }
+
+    return vertices;
+}
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, vertexShaderSource);
@@ -57,47 +64,19 @@ gl.compileShader(fragmentShader);
 const shaderProgram = gl.createProgram();
 gl.attachShader(shaderProgram, vertexShader);
 gl.attachShader(shaderProgram, fragmentShader);
-gl.linkProgram(shaderProgram)
+gl.linkProgram(shaderProgram);
 
 gl.useProgram(shaderProgram);
 
-const squaresDiameter1 = 0.667;
-const squareCenters1 = [0, 0];
-let vertices = getSquareVertices(squareCenters1, squaresDiameter1);
-
-const squaresDiameter2 = squaresDiameter1 / 3;
-const squareCenters2 = getNewCenters(squareCenters1, squaresDiameter1);
-for(let i = 0; i < squareCenters2.length; i += 2){
-    const tempCenter = [squareCenters2[i], squareCenters2[i + 1]];
-    vertices = vertices.concat(getSquareVertices(tempCenter, squaresDiameter2))
-}
-
-const squaresDiameter3 = squaresDiameter2 / 3;
-let squareCenters3 = [];
-for(let i = 0; i < squareCenters2.length; i += 2){
-    const tempCenter = [squareCenters2[i], squareCenters2[i + 1]];
-    squareCenters3 = squareCenters3.concat(getNewCenters(tempCenter, squaresDiameter2));
-}
-for(let i = 0; i < squareCenters3.length; i += 2){
-    const tempCenter = [squareCenters3[i], squareCenters3[i + 1]];
-    vertices = vertices.concat(getSquareVertices(tempCenter, squaresDiameter3))
-}
-
-const squaresDiameter4 = squaresDiameter3 / 3;
-let squareCenters4 = [];
-for(let i = 0; i < squareCenters3.length; i += 2){
-    const tempCenter = [squareCenters3[i], squareCenters3[i + 1]];
-    squareCenters4 = squareCenters4.concat(getNewCenters(tempCenter, squaresDiameter3));
-}
-for(let i = 0; i < squareCenters4.length; i += 2){
-    const tempCenter = [squareCenters4[i], squareCenters4[i + 1]];
-    vertices = vertices.concat(getSquareVertices(tempCenter, squaresDiameter4))
-}
+const initialSquareLength = 2.0;
+const initialSquareCenter = [0, 0];
+const numberOfSteps = 5; // Manipulate this to adjust the number of steps in the carpet
+const vertices = drawSierpinski(initialSquareCenter, initialSquareLength, numberOfSteps);
 
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
 gl.clear(gl.COLOR_BUFFER_BIT);
 
-// add vertices to draw
+// Add vertices to draw
 const vertexBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
@@ -106,9 +85,8 @@ const aPositionLocation = gl.getAttribLocation(shaderProgram, 'aPosition');
 gl.enableVertexAttribArray(aPositionLocation);
 gl.vertexAttribPointer(aPositionLocation, 2, gl.FLOAT, false, 0, 0);
 
-// set color for all vertices to share
+// Set color for all vertices to share
 const uColorLocation = gl.getUniformLocation(shaderProgram, 'uColor');
-gl.uniform4fv(uColorLocation, [0.2, 0.6, 0.9, 1.0]); // red color
+gl.uniform4fv(uColorLocation, [0.2, 0.6, 0.9, 1.0]); 
 
 gl.drawArrays(gl.TRIANGLES, 0, vertices.length / 2);
-
